094864f9dc5e5b28d657d02d79877cdb11c37012
FahadQurashi49 <fahad.ahmad049@gmail.com>
2017-09-08 21:39:39 +0500

    move MainActivity to activity package

--- app/src/main/java/com/qfree/qfree_facilitator/activity/MainActivity.java
+++ app/src/main/java/com/qfree/qfree_facilitator/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.qfree.qfree_facilitator;
+package com.qfree.qfree_facilitator.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.qfree.qfree_facilitator.R;
 
 public class MainActivity extends AppCompatActivity {
 
